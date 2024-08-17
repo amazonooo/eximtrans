dcd7f5bf8640c7c60beac61643b8a658adbf8270
amazonooo <garikogannisyan.9@gmail.com>
2024-08-17 18:29:31 +0300

    new animation on card

--- src/components/ui/services/ServicesCard.tsx
+++ src/components/ui/services/ServicesCard.tsx
@@ -16,7 +16,7 @@ const ServicesCard: FC<IServicesCard> = ({ id, title, image, href, anim }) => {
   return (
 		<Link
 			href={href}
-			className={cn(`bg-white cursor-pointer rounded-lg h-[356px] w-[300px] sm:w-[450px] md:w-[350px] lg:w-[425px] xl:w-[650px] group shadow hover:-translate-y-2 duration-300`, anim)}
+			className={cn(`bg-white cursor-pointer rounded-lg h-[356px] w-[300px] sm:w-[450px] md:w-[350px] lg:w-[425px] xl:w-[650px] group hover:-translate-y-2 hover:shadow-xl duration-300 transition-all`, anim)}
 		>
 			<div className='pt-8 pb-4 px-8 flex flex-col justify-between h-full'>
 				<h2 className='text-xl md:text-2xl'>{title}</h2>
