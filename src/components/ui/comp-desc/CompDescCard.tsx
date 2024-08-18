f9f342004151d1f2af98ece95c8a558bbc82e74a
amazonooo <garikogannisyan.9@gmail.com>
2024-08-19 01:09:11 +0300

    again

--- src/components/ui/comp-desc/CompDescCard.tsx
+++ src/components/ui/comp-desc/CompDescCard.tsx
@@ -8,59 +8,57 @@ import { useSpring, animated } from 'react-spring'
 interface ICompDescCard {
   id?: string
   title: string
-  count: string
+  count: number
   desc: string
 }
 
 gsap.registerPlugin(ScrollTrigger)
 
 const CompDescCard: FC<ICompDescCard> = ({ id, title, count, desc }) => {
-	const cardRef = useRef<HTMLDivElement>(null)
+  const cardRef = useRef<HTMLDivElement>(null)
+	const { number } = useSpring({
+		from: { number: 0 },
+		number: count,
+		config: { tension: 200, friction: 20 },
+	})
 
-  function Number({ n }: any) {
-		const { number } = useSpring({
-			from: { number: 0 },
-			number: n,
-			delay: 200,
-			config: { mass: 1, tension: 20, friction: 10 },
-		})
-		return <animated.div>{number.to(n => n.toFixed(0))}</animated.div>
-	}
+  useEffect(() => {
+		const cardElement = cardRef.current
 
-	 useEffect(() => {
-			const card = cardRef.current
+		if (cardElement) {
+			gsap.fromTo(
+				cardElement,
+				{ opacity: 0, y: 50 },
+				{
+					opacity: 1,
+					y: 0,
+					scrollTrigger: {
+						trigger: cardElement,
+						start: 'top 80%',
+						toggleActions: 'play none none none',
+					},
+					duration: 1,
+				}
+			)
+		}
 
-			if (card) {
-				ScrollTrigger.create({
-					trigger: card,
-					start: 'top 80%',
-					onEnter: () => gsap.to(card, { opacity: 1, duration: 0.5 }),
-					onEnterBack: () => gsap.to(card, { opacity: 1, duration: 0.5 }),
-					onLeave: () => gsap.to(card, { opacity: 0, duration: 0.5 }),
-					onLeaveBack: () => gsap.to(card, { opacity: 0, duration: 0.5 }),
-				})
-			}
-
-			return () => ScrollTrigger.getAll().forEach(trigger => trigger.kill())
-		}, [])
+		return () => {
+			ScrollTrigger.getAll().forEach(trigger => trigger.kill())
+		}
+	}, [])
 
 	return (
-		<article ref={cardRef} className='bg-white h-[316px] w-[316px] sm:h-[450px] sm:w-[450px] md:h-[260px] md:w-[260px] lg:h-[320px] lg:w-[320px] xl:h-[430px] xl:w-[430px] group shadow rounded-full opacity-0'>
+		<article
+			ref={cardRef}
+			className='bg-white h-[316px] w-[316px] sm:h-[450px] sm:w-[450px] md:h-[260px] md:w-[260px] lg:h-[320px] lg:w-[320px] xl:h-[430px] xl:w-[430px] group shadow rounded-full'
+		>
 			<div className='flex flex-col items-center h-full px-8 lg:px-12 py-10 lg:py-14 xl:py-24 gap-y-16 md:gap-y-4 xl:gap-y-16'>
 				<h2 className='text-3xl lg:text-4xl text-[#930D40] text-center whitespace-nowrap'>
 					{title}
 				</h2>
 				<div className='flex flex-col items-center gap-y-4 text-center'>
 					<div className='text-3xl sm:text-4xl md:text-4xl lg:text-7xl font-semibold'>
-						{id === '1' ? (
-							<Number n={~99} />
-						) : id === '2' ? (
-							<Number n={~2499} />
-						) : id === '3' ? (
-							<Number n={~1999} />
-						) : (
-							''
-						)}
+						<animated.div>{number.to(n => n.toFixed(0))}</animated.div>
 					</div>
 
 					<p className='text-lg lg:text-xl'>{desc}</p>
