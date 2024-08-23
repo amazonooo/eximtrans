a2d41650f9e0fa79412919e53a9f7639e9a8d992
amazonooo <garikogannisyan.9@gmail.com>
2024-08-24 00:37:10 +0300

    overflow hidden on mobile

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -5,7 +5,7 @@ import GlobeComponent from '@/components/layout/Geography'
 
 export default function Home() {
   return (
-    <main className='w-full h-full flex flex-col overflow-clip px-5 sm:px-10'>
+    <main className='w-full h-full flex flex-col overflow-y-clip overflow-x-hidden px-5 sm:px-10'>
       <Hero />
       <Services />
       <CompDesc />
