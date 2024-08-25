8a02e90547f5645a15d6d60670f1b03eccf69275
amazonooo <garikogannisyan.9@gmail.com>
2024-08-25 18:33:48 +0300

    form

--- src/app/career/components/CareerForm.tsx
+++ src/app/career/components/CareerForm.tsx
@@ -1,43 +1,45 @@
 'use client'
 
-import Input from '@/components/ui/input/Input'
-import { inputData } from '@/components/ui/input/inputData'
 import { useRef, useState } from 'react'
-import { motion } from 'framer-motion'
+import { toast, ToastContainer } from 'react-toastify'
 import emailjs from '@emailjs/browser'
-import { slideIn } from '@/utils/motion'
+import 'react-toastify/dist/ReactToastify.css'
 
-// 1wcWUQA2CLAzji-jz
-// template_i5vpd6d
-// service_aixfu79
+export default function CareerForm() {
+	const formRef = useRef<HTMLFormElement>(null)
 
-export default function CareerForm() {  
-  const formRef = useRef<HTMLFormElement>(null)
-
-  const [loading, setLoading] = useState(false)
-  const [form, setForm] = useState({
+	const [loading, setLoading] = useState(false)
+	const [form, setForm] = useState({
 		name: '',
 		surname: '',
 		patronymic: '',
 		email: '',
 		phone: '',
 		city: '',
-		cityEmployment: '',
 		preferredCity: '',
 		direction: '',
 		specialization: '',
 		resume: '',
 	})
 
-  const handleChange = (e: any) => {
-    const { name, value } = e.target
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target
+		setForm({ ...form, [name]: value })
+	}
 
-    setForm({ ...form, [name]: value })
-  }
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault()
-    setLoading(true)
+		// Проверка на заполнение всех полей
+		const isFormComplete = Object.values(form).every(
+			field => field.trim() !== ''
+		)
+		if (!isFormComplete) {
+			toast.error('Заполните все поля!')
+			return
+		}
+
+		setLoading(true)
 
 		const templateParams = {
 			from_name: form.name,
@@ -46,164 +48,147 @@ export default function CareerForm() {
 			email: form.email,
 			phone: form.phone,
 			city: form.city,
-			cityEmployment: form.cityEmployment,
 			preferredCity: form.preferredCity,
 			direction: form.direction,
 			specialization: form.specialization,
 			resume: form.resume,
 		}
 
-    emailjs.send(
-			'service_aixfu79',
-			'template_i5vpd6d',
-			templateParams,
-			'1wcWUQA2CLAzji-jz'
-		)
-    .then(
-      (response) => {
-				setLoading(false);
-				alert('Форма успешно отправлена!');
-				setForm({
-					name: '',
-					surname: '',
-					patronymic: '',
-					email: '',
-					phone: '',
-					city: '',
-					cityEmployment: '',
-					preferredCity: '',
-					direction: '',
-					specialization: '',
-					resume: '',
-				});
-    	}, (error) => {
-      setLoading(false)
-			console.error('Ошибка при отправке:', error)
-			alert('Произошла ошибка при отправке формы.')
-    })
-  }
+		emailjs
+			.send(
+				'service_aixfu79',
+				'template_i5vpd6d',
+				templateParams,
+				'1wcWUQA2CLAzji-jz'
+			)
+			.then(
+				response => {
+					setLoading(false)
+					toast.success('Анкета успешно отправлена!')
+					setForm({
+						name: '',
+						surname: '',
+						patronymic: '',
+						email: '',
+						phone: '',
+						city: '',
+						preferredCity: '',
+						direction: '',
+						specialization: '',
+						resume: '',
+					})
+				},
+				error => {
+					setLoading(false)
+					console.error('Ошибка при отправке:', error)
+					toast.error('Что-то пошло не так')
+				}
+			)
+	}
 
-  // async function handleSubmit(e: any) {
-  //   e.preventDefault()
-  //   setLoading(true)
-
-  //   const data = {
-  //     name: String(e.target.name.value),
-  //     email: String(e.target.email.value),
-  //     message: String(e.target.message.value),
-  //   }
-
-  //   console.log(data)
-  //   const response = await fetch('/api/sendEmail', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify(data)
-  //   })
-    
-  //   if (response.ok) {
-  //     console.log('Message sent successfully')
-  //     setLoading(false)
-
-  //     e.target.name.value = ''
-  //     e.target.email.value = ''
-  //     e.target.message.value = ''
-  //   }
-  //   if (!response.ok) {
-  //     console.log('Error sending message')
-  //     setLoading(false)
-  //   }
-  // }
-
-  return (
+	return (
 		<div>
-			<form
-				ref={formRef}
-				onSubmit={handleSubmit}
-				className='grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-10'
-			>
-				<input
-					type='text'
-					name='name'
-					value={form.name}
-					onChange={handleChange}
-					placeholder='Имя'
-				/>
-				<input
-					type='text'
-					name='surname'
-					value={form.surname}
-					onChange={handleChange}
-					placeholder='Фамилия'
-				/>
-				<input
-					type='text'
-					name='patronymic'
-					value={form.patronymic}
-					onChange={handleChange}
-					placeholder='Отчество'
-				/>
-				<input
-					type='email'
-					name='email'
-					value={form.email}
-					onChange={handleChange}
-					placeholder='Электронная почта'
-				/>
-				<input
-					type='tel'
-					name='phone'
-					value={form.phone}
-					onChange={handleChange}
-					placeholder='Телефон'
-				/>
-				<input
-					type='text'
-					name='city'
-					value={form.city}
-					onChange={handleChange}
-					placeholder='Город фактического проживания'
-				/>
-				<input
-					type='text'
-					name='cityEmployment'
-					value={form.cityEmployment}
-					onChange={handleChange}
-					placeholder='Город фактического проживания (ещё раз)'
-				/>
-				<input
-					type='text'
-					name='preferredCity'
-					value={form.preferredCity}
-					onChange={handleChange}
-					placeholder='Предпочтительный город присутствия Экспимтранс'
-				/>
-				<input
-					type='text'
-					name='direction'
-					value={form.direction}
-					onChange={handleChange}
-					placeholder='Направление'
-				/>
-				<input
-					type='text'
-					name='specialization'
-					value={form.specialization}
-					onChange={handleChange}
-					placeholder='Специализация'
-				/>
-				<textarea
-					name='resume'
-					value={form.resume}
-					onChange={handleChange}
-					placeholder='Резюме'
-				></textarea>
-
+			<ToastContainer
+				position='top-center'
+				autoClose={3000}
+				limit={3}
+				hideProgressBar={true}
+				closeOnClick
+				pauseOnHover={false}
+			/>
+			<form ref={formRef} onSubmit={handleSubmit} className=''>
+				<div className='grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-10'>
+					<input
+						type='text'
+						name='name'
+						value={form.name}
+						onChange={handleChange}
+						className='focus:outline-primary-red outline-none rounded-lg w-full p-3 min-w-[300px] sm:min-w-[450px] md:min-w-[300px] lg:min-w-[360px] xl:min-w-[560px] transition-all duration-300'
+						placeholder='Имя'
+					/>
+					<input
+						type='text'
+						name='surname'
+						value={form.surname}
+						onChange={handleChange}
+						className='focus:outline-primary-red outline-none rounded-lg w-full p-3 min-w-[300px] sm:min-w-[450px] md:min-w-[300px] lg:min-w-[360px] xl:min-w-[560px] transition-all duration-300'
+						placeholder='Фамилия'
+					/>
+					<input
+						type='text'
+						name='patronymic'
+						value={form.patronymic}
+						onChange={handleChange}
+						className='focus:outline-primary-red outline-none rounded-lg w-full p-3 min-w-[300px] sm:min-w-[450px] md:min-w-[300px] lg:min-w-[360px] xl:min-w-[560px] transition-all duration-300'
+						placeholder='Отчество'
+					/>
+					<input
+						type='email'
+						name='email'
+						value={form.email}
+						onChange={handleChange}
+						className='focus:outline-primary-red outline-none rounded-lg w-full p-3 min-w-[300px] sm:min-w-[450px] md:min-w-[300px] lg:min-w-[360px] xl:min-w-[560px] transition-all duration-300'
+						placeholder='Электронная почта'
+					/>
+					<input
+						type='tel'
+						name='phone'
+						value={form.phone}
+						onChange={handleChange}
+						className='focus:outline-primary-red outline-none rounded-lg w-full p-3 min-w-[300px] sm:min-w-[450px] md:min-w-[300px] lg:min-w-[360px] xl:min-w-[560px] transition-all duration-300'
+						placeholder='Телефон'
+					/>
+					<input
+						type='text'
+						name='city'
+						value={form.city}
+						onChange={handleChange}
+						className='focus:outline-primary-red outline-none rounded-lg w-full p-3 min-w-[300px] sm:min-w-[450px] md:min-w-[300px] lg:min-w-[360px] xl:min-w-[560px] transition-all duration-300'
+						placeholder='Город фактического проживания'
+					/>
+					<input
+						type='text'
+						name='preferredCity'
+						value={form.preferredCity}
+						onChange={handleChange}
+						className='focus:outline-primary-red outline-none rounded-lg w-full p-3 min-w-[300px] sm:min-w-[450px] md:min-w-[300px] lg:min-w-[360px] xl:min-w-[560px] transition-all duration-300 truncate'
+						placeholder='Предпочтительный город присутствия Эксимтранс'
+					/>
+					<input
+						type='text'
+						name='direction'
+						value={form.direction}
+						onChange={handleChange}
+						className='focus:outline-primary-red outline-none rounded-lg w-full p-3 min-w-[300px] sm:min-w-[450px] md:min-w-[300px] lg:min-w-[360px] xl:min-w-[560px] transition-all duration-300'
+						placeholder='Направление'
+					/>
+					<input
+						type='text'
+						name='specialization'
+						value={form.specialization}
+						onChange={handleChange}
+						className='focus:outline-primary-red outline-none rounded-lg w-full p-3 min-w-[300px] sm:min-w-[450px] md:min-w-[300px] lg:min-w-[360px] xl:min-w-[560px] transition-all duration-300'
+						placeholder='Специализация'
+					/>
+					<input
+						type='text'
+						name='resume'
+						value={form.resume}
+						onChange={handleChange}
+						className='focus:outline-primary-red outline-none rounded-lg w-full p-3 min-w-[300px] sm:min-w-[450px] md:min-w-[300px] lg:min-w-[360px] xl:min-w-[560px] transition-all duration-300'
+						placeholder='Резюме'
+					/>
+				</div>
+				<div className='mt-10 lg:mt-20 font-medium w-full flex items-center justify-center'>
+					<button
+						type='submit'
+						disabled={loading}
+						className='bg-white cursor-pointer border border-primary-red text-center px-8 py-5 rounded-xl hover:bg-primary-red hover:text-white transition-colors duration-500 disabled:cursor-not-allowed disabled:bg-neutral-200 disabled:border-none'
+					>
+						Отправить анкету
+					</button>
+				</div>
 			</form>
-				<button type='submit' className='text-center w-fit' disabled={loading}>
-					{loading ? 'Отправка...' : 'Отправить анкету'}
-				</button>
 		</div>
 	)
 }
