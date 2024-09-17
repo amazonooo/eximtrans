7d02c0e1970bb89fc53d0a7f9b77e5c17bbb88b5
amazonooo <garikogannisyan.9@gmail.com>
2024-09-17 16:48:03 +0300

    one more fixed

--- src/app/avtomobilnye-perevozki/components/AvtoContact.tsx
+++ src/app/avtomobilnye-perevozki/components/AvtoContact.tsx
@@ -10,8 +10,6 @@ const AvtoContact: FC = () => {
 			link='sergey240980@mail.ru'
 			email='sergey240980@mail.ru'
 			phone='+7 (926) 071-56-95'
-			image2='/'
-			link2=''
 		/>
 	)
 }
--- src/app/remont-vagonov/components/RemontContact.tsx
+++ src/app/remont-vagonov/components/RemontContact.tsx
@@ -10,11 +10,6 @@ const RemontContact: FC = () => {
 			email='mamedov@abrail.ru'
 			link='mamedov@abrail.ru'
 			image='/ava-2.jpg'
-			image2='/ava-2.jpg'
-			name2='ДР Тетнев Александр Евгеньевич'
-			phone2='+7 (495) 269-85-54'
-			email2='tae@abrail.ru'
-			link2='tae@abrail.ru'
 		/>
 	)
 }
